41649605eb1536e66c611c59578039edf7c4af8b
73-hana <hanaprgrm1997@gmail.com>
2024-01-26 18:58:54 +0900

    SFCとMDを併用できるよう設定

---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Sample</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, laudantium. Quo ipsa, consectetur vero adipisci iusto quas. Dolores velit inventore at blanditiis eius maxime, error eaque excepturi sequi cumque a?</p>
+</template>
