fed9f1d90cb9874e43b0c22d822039ed92e77a6f
73-hana <hanaprgrm1997@gmail.com>
2024-02-01 22:14:09 +0900

    経済学における諸定義メモ

---
+++ pages/economics/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>経済学</h1>
+  <section>
+    <h2>政治学の基礎を学ぶ</h2>
+    <p>公務員試験の範囲に沿って、経済学の基礎を勉強します。</p>
+    <ul>
+      <li>準備中</li>
+    </ul>
+  </section>
+  <section>
+    <h2>経済学の本を読む</h2>
+    <p>古典から現代の本まで、気になるものを読む</p>
+    <ul>
+      <li><NuxtLink to="/economics/book/definitions-in-political-economy">マルサス『経済学における諸定義』</NuxtLink></li>
+    </ul>
+  </section>
+  <p><NuxtLink to="/">トップ</NuxtLink></p>
+</template>
