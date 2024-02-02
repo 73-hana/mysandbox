fa7690b4d7599b86a6b4613f153f497b1d8ba761
73-hana <hanaprgrm1997@gmail.com>
2024-02-02 21:37:09 +0900

    faviconの修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     baseURL: '/myblog/',
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/myblog/favicon.ico' }
       ],
     }
   },
