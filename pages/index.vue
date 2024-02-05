8f7744446c78c668ca2983087510741777560c66
73-hana <hanaprgrm1997@gmail.com>
2024-02-06 00:03:37 +0900

    経済学のメモ、プログラミングのメモ

--- pages/index.vue
+++ pages/index.vue
@@ -7,4 +7,7 @@
     <li><NuxtLink to="/programming/">プログラミング</NuxtLink></li>
   </ul>
   <p>内容に間違いがあれば<a href="https://github.com/73-hana/mysandbox/issues/new" target="_blank" rel="noreferrer noopener">GitHubのIssue</a>からお知らせください。</p>
+  <ul>
+    <li><a href="https://www.codewars.com/users/73_hana">Codewars</a></li>
+  </ul>
 </template>
