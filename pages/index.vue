37ee055cd08c52a4dc62e2d4518b24f30454b5b1
73-hana <hanaprgrm1997@gmail.com>
2024-02-11 21:47:44 +0900

    パスを通す

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <li><NuxtLink to="/economics">経済学</NuxtLink></li>
     <li><NuxtLink to="/programming/">プログラミング</NuxtLink></li>
   </ul>
+  <p><NuxtLink to="/public-file/la-distinction-simplified">ディスタンクシオンメモ</NuxtLink></p>
   <p>内容に間違いがあれば<a href="https://github.com/73-hana/mysandbox/issues/new" target="_blank" rel="noreferrer noopener">GitHubのIssue</a>からお知らせください。</p>
   <ul>
     <li><a href="https://www.codewars.com/users/73_hana">Codewars</a></li>
